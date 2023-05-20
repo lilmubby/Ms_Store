4dbb403780a0fdd359967b09afd32b0640e8c787
Mubarak Yahaya <ymubarak247@gmail.com>
2023-05-20 23:24:18 +0100

    refactor product filter function for search query

--- src/Shop/Product.jsx
+++ src/Shop/Product.jsx
@@ -5,27 +5,24 @@ import { useSelector } from "react-redux";
 //
 
 const Product = () => {
+  const { productItems, searchQuery } = useSelector((state) => state.products);
 
-  const {productItems, searchQuery} = useSelector(state => state.products);
-
-      
   return (
     <ul>
-      {productItems.filter(items => {
-        
-        return items.brand.toLowerCase().includes(searchQuery.toLowerCase())
-        
-      })
-      .map((item) => (
-        <ProductItem
-          key={item.id}
-          id={item.id}
-          img={item.imgSrc}
-          name={item.name}
-          brand={item.brand}
-          price={item.price}
-        />
-      ))}
+      {productItems
+        .filter((items) =>
+          items.brand.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+        .map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            img={item.imgSrc}
+            name={item.name}
+            brand={item.brand}
+            price={item.price}
+          />
+        ))}
     </ul>
   );
 };
